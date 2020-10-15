e34e8474ca88ff621f88a1a7d180bb22c4111872
aurelrichard <aurelrichard00@gmail.com>
2020-10-15 20:59:49 +0200

    manually merged master branch into main branch

---
+++ index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>qrs</title>
+  <meta name="description" content="qrs">
+  <meta name="author" content="Jordi">
+  <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
+</head>
+<body>
+<?php
+require('config.php');
+// If form submitted, insert values into the database.
+if (isset($_REQUEST['name']) && isset($_REQUEST['privacy'])){
+    $username = stripslashes($_REQUEST['name']);
+    $username = mysqli_real_escape_string($conn,$username); 
+    $email = stripslashes($_REQUEST['email']);
+    $email = mysqli_real_escape_string($conn,$email);
+    $sql_e = "SELECT * FROM `users` WHERE `mail`='$email'";
+    $sql_u = "SELECT * FROM `users` WHERE `name`='$username'";
+    $res_e = mysqli_query($conn, $sql_e);
+    $res_u = mysqli_query($conn, $sql_u);
+    if(mysqli_num_rows($res_e) > 0 && mysqli_num_rows($res_u) > 0 ){
+        echo "<div><h3>User already resgistered</h3></div>";	
+        setcookie('name', $username);
+        setcookie('email', $email);
+  	}
+	else if(mysqli_num_rows($res_e) > 0 && mysqli_num_rows($res_u) < 1 ){
+        echo "<div><h3>Sorry... email already taken</h3></div>";	
+      }
+    else{
+        setcookie('name', $username);
+        setcookie('email', $email);
+        $query = "INSERT INTO `users`(`name`, `mail`) VALUES ('$username', '$email')";
+        $result = mysqli_query($conn,$query);
+        if($result){
+            echo "<div><h3>You are registered successfully.</h3></div>";
+        }
+    }
+}else{
+    echo "<div><h3>Please check all the fields and accept our policies</h3></div>";
+}
+// get params from qr url 
+if (isset($_GET['id'])) {
+    echo $_GET['id'];
+    setcookie('id', $_GET['id']);
+
+
+} else {
+    // Fallback behaviour goes here
+}
+?>
+  <div class="form">
+    <h1>Registration</h1>
+    <form name="registration" action="" method="post">
+    <input type="text" name="name" placeholder="Username" required />
+    <input type="email" name="email" placeholder="Email" required />
+    <div>
+        <input type="radio" id="privacy" name="privacy" value="huey">
+        <label for="huey">Accept privacy policy</label>
+    </div>
+    <input type="submit" name="submit" value="Register" />
+    </form>
+</div>
+ <script>
+
+
+console.log(document.cookie);
+</script>
+
+ </script>
+</body>
+</html>
+
---
+++ index2.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>qrs</title>
+  <meta name="description" content="qrs">
+  <meta name="author" content="Jordi">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
+</head>
+<body>
+<?php
+require('config.php');
+//check user exist
+if(count($_COOKIE) > 0) {
+    //cookies are enabled
+    if(!isset($_COOKIE['userId'])) { 
+        //user has no ID go to Register Form
+        echo "no cookie";
+        //rember the qr url
+        if (isset($_GET['id'])) {
+            setcookie('prevUrl', $_GET['id']);
+            header("Location: http://localhost/qrs/form.html"); 
+            exit();
+        }
+      
+       
+    }else{
+        
+        //user has an Id, has been on the site 
+        $userId = $_COOKIE['userId'];
+        echo "welcome back id: " . $userId;
+        // get params from qr url 
+        if (isset($_GET['id'])) {
+            $qr = $_GET['id'];
+            //check visited de Qr parameter id
+            $sql_id= "SELECT `$qr` FROM `codes` WHERE `id`='$userId'";
+            $result= mysqli_query($conn, $sql_id);
+            while ($row = $result->fetch_assoc()) {
+                $qrResult= $row[$qr];
+            }
+            if($qrResult>0){
+                //user visited the QR- show error ups screen
+                echo "you allready scanned that QR";
+            }else{
+                //update qrs database to visited qr
+                $updateQR = mysqli_query($conn, "UPDATE `codes` SET `$qr`=1 WHERE `id`='$userId'");
+                //get user points
+                $sql_points= "SELECT `points` FROM `users` WHERE `id`='$userId'";
+                $resultPoints= mysqli_query($conn, $sql_points);
+                while ($row = $resultPoints->fetch_assoc()) {
+                    $pointsResult= $row['points'];
+                }
+                // update points of user
+                if($qr<6){
+                    $pointsResult+=10;
+                }else{
+                    $pointsResult+=15;
+                }
+                $updatePoints = mysqli_query($conn, "UPDATE `users` SET `points`='$pointsResult' WHERE `id`='$userId'");
+                echo "points_ " .  $pointsResult;
+                setcookie('points', $pointsResult);
+                //show reward(check json to know points, and content)
+                //display de points and user
+            }
+
+        } else {
+            // Fallback behaviour goes here
+        }   
+    }
+  } else {
+   //Cookies are disabled
+  }
+?>
+<div id="menu" style="font-size:20px"></div>
+
+
+
+<script>
+
+    //Get data for menu, points and username from cookies
+    var userPoints = getCookieValue("points");
+        var userName = getCookieValue("name");
+        document.getElementById("menu").innerHTML = "Hi: " + userName + "! Your points:" + userPoints;
+
+        function getCookieValue(a) {
+            var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
+            return b ? b.pop() : '';
+        }
+
+ </script>
+</body>
+</html>
+
